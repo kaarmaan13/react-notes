c7dc2a11fcd9bdf69c68e48cc3abc031785572d4
Karman Karman <karman131313@gmail.com>
2021-11-10 21:25:30 +0100

    Pass notes to App from props

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import "./styles.css"
-import {Note} from './Note.js'
+import "./styles.css";
+import { Note } from "./Note.js";
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
-
-export default function App() {
-  if (typeof notes === "undefined" || notes.length === 0) {
-    return "No hay notas que mostrar."
-  } else {
-      return (
-        <div className="App">
-          { notes.map(note =>(
-            <Note {...note} key={note.id} />
-          )) }
-        </div>
-      )
-    }
+export default function App({ notes = [] }) {
+  return (
+    <>
+      <div><h1>Notes</h1></div>
+      <div>
+        {notes.map((note) => (
+          <Note {...note} key={note.id} />
+        ))}
+      </div>
+    </>
+  );
 }
